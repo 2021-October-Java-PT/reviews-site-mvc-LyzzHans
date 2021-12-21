dadf9056bd460811c5a8a589e96956765dc3052c
LyzzHans <lyzz.hans@gmail.com>
2021-12-21 16:16:01 -0500

    TURNING IN HOORAY

--- src/main/java/org/wecancoeit/reviews/ReviewRepository.java
+++ src/main/java/org/wecancoeit/reviews/ReviewRepository.java
@@ -12,11 +12,11 @@ public class ReviewRepository {
 
     Map<Long, Review> reviewsList = new HashMap<>();
 
-        Review lasTotallyAwesome = new Review(1L, "LAs Totally Awesome", "images/las.jpg", "all purpose cleaner", "spray bottle","Smelly, but cleans tough stains");
-        Review persil = new Review(2L, "Persil", "images/persil.jpg", "laundry detergent", "liquid detergent","Expensive, but magic on laundry");
-        Review fabuloso = new Review(3L, "Fabuloso", "images/fab.jpg", "all purpose liquid", "all surface cleaner","Die hard fan, add a cap of bleach");
-        Review odoBan = new Review(4L, "OdoBan", "images/odo.jpg", "disinfectant", "spray pump disinfectant","smells so much better than lysol");
-        Review dawnPowerwash = new Review(5L, "Dawn Powerwash", "images/dp.jpg", "dish soap", "spray pump dish soap","Not just for dishes, cleans tubs and showers too!");
+        Review lasTotallyAwesome = new Review(1L, "LAs Totally Awesome", "https://i5.walmartimages.com/asr/adf40a19-90c0-492c-ac84-2bddb4284200_1.aedec1ba31b6a16e50c77f67dffa6b06.jpeg?odnHeight=612&odnWidth=612&odnBg=FFFFFF", "all purpose cleaner", "spray bottle","Smelly, but cleans tough stains");
+        Review persil = new Review(2L, "Persil", "https://target.scene7.com/is/image/Target/GUEST_4b7ffc55-8ef4-4866-88f2-5cd6bd780a10?wid=325&hei=325&qlt=80&fmt=pjpeg", "laundry detergent", "liquid detergent","Expensive, but magic on laundry");
+        Review fabuloso = new Review(3L, "Fabuloso", "https://images.thdstatic.com/productImages/52b00cde-1899-46b5-8d5f-b4c3ae0a8590/svn/fabuloso-all-purpose-cleaners-53122-64_1000.jpg", "all purpose liquid", "all surface cleaner","Die hard fan, add a cap of bleach");
+        Review odoBan = new Review(4L, "OdoBan", "https://i5.walmartimages.com/asr/da353fd9-147f-4daf-9e24-230fe3a09b50_1.0d396ee643c008e625dfd2c4311a99ad.jpeg", "disinfectant", "spray pump disinfectant","smells so much better than lysol");
+        Review dawnPowerwash = new Review(5L, "Dawn Powerwash", "https://target.scene7.com/is/image/Target/GUEST_f0742ee0-4f75-4c9a-a649-ff2ceac9e0f1?wid=325&hei=325&qlt=80&fmt=pjpeg", "dish soap", "spray pump dish soap","Not just for dishes, cleans tubs and showers too!");
 
     public ReviewRepository() {
 
